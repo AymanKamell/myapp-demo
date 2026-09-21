0494aa390a7b10b377f63a241d56f0243a020c45
Ayman Kamel <aymankamel20k@gmail.com>
2026-09-21 19:27:02 +0300

    Add backend unit tests

---
+++ backend/backend-application/tests/test_app.py
@@ -0,0 +1,19 @@
+from app import app
+
+
+def test_health():
+    client = app.test_client()
+
+    response = client.get("/health")
+
+    assert response.status_code == 200
+    assert response.json["status"] == "healthy"
+
+
+def test_message():
+    client = app.test_client()
+
+    response = client.get("/api/message")
+
+    assert response.status_code == 200
+    assert response.json["message"] == "Hello from the Kubernetes backend!"
